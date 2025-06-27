a6b5d097da4515ee446a8807fc7d7189bb97ae79
Tony Saes <saestony.dev@gmail.com>
2025-06-27 10:18:30 +0200

    étape 2 : création objet hercule et fillProfil ok

--- js/exo.js
+++ js/exo.js
@@ -1,1 +1,11 @@
 console.log('test 1...2...1...2');
+
+const hercule = {
+    name: 'Hercule',
+    job: 'Demi-dieu',
+    age: 35,
+    department: 75,
+    arm: 60.5,
+    inRelationship: true,
+};
+base.fillProfil(hercule)
