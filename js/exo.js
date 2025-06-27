bb0855f2b460134a045dccde55e9f66cd3254c25
Tony Saes <saestony.dev@gmail.com>
2025-06-27 10:31:44 +0200

    étape 4 : création élément title dom via JS ok

--- js/exo.js
+++ js/exo.js
@@ -1,5 +1,6 @@
+//Etape 1 : Importer le module
 console.log('test 1...2...1...2');
-
+//Etape 2 : Créer un objet
 const hercule = {
     name: 'Hercule',
     job: 'Demi-dieu',
@@ -9,7 +10,15 @@ const hercule = {
     inRelationship: true,
 };
 base.fillProfil(hercule)
-
+// Etape 3 : création tableau
 const friends = ['Jupiter', 'Junon', 'Alcmène', 'Déjanire'];
 base.printFriends(friends);
 base.setBestFriend(friends[0]);
+
+// Etape 4 : Création DOM
+
+const newTitle = document.createElement('h1');
+newTitle.textContent = "Vous consultez le profil de Hercule";
+newTitle.classList.add('banner__title');
+document.querySelector("#header-banner").append(newTitle);
+
